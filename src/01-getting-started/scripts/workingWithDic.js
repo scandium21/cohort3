b4a08dd792e59e72ae16981c3503ef63b722244b
SC <siliang.chang@gmail.com>
2019-10-11 18:59:56 -0600

    done with working with dics, comp 100 completed

---
+++ src/01-getting-started/scripts/workingWithDic.js
@@ -0,0 +1,39 @@
+let prov = {
+    AB:"Alberta",
+    BC:"British Columbia",
+    MB:"Manitoba",
+    NB:"New Brunswick",
+    NL:"Newfoundland and Labrador",
+    NS:"Nova Scotia",
+    ON:"Ontario",
+    PE:"Prince Edward Island",
+    SK:"Saskatchewan",
+    NT:"Northwest Territories",
+    NU:"Nunavut",
+    YT:"Yukon",
+};
+
+const lookUp = () => {
+    let input = idDicInput.value.toUpperCase();
+    if (input.length === 0) {
+        printMessage("Please enter province code ~");
+        return;
+    }
+    if (prov[input] === undefined) {
+        printMessage("Province not found by that code.");
+        return;
+    }
+    printMessage(`${input} - ${prov[input]}`);
+    return prov[input];
+}
+
+const printMessage = (string) => {
+    idDicMessageArea.textContent = string;
+}
+
+let lookUpBtn = document.querySelector(".dicBtns");
+
+lookUpBtn.addEventListener("click", ()=>{
+    lookUp();
+});
+
