810fa55dd6bd6e1a8e2b289d1d6c0d9ce9fca25c
SC <siliang.chang@gmail.com>
2019-10-25 11:02:54 -0600

    minor change in daily.js and 03-object index.html

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -29,11 +29,10 @@ const functions = {
       The new function you are writing today will call makeEmailObj that you have written in a previous exercise
     */
   loopStaff: arr => {
-    let list = [];
-    arr.forEach(item => {
-      let email = functions.makeEmailObj(item);
-      list.push(email);
+    let list = arr.map(item => {
+      return functions.makeEmailObj(item);
     });
+
     return list;
   },
 
@@ -73,11 +72,11 @@ const functions = {
   // arr.forEach(callback(currentValue [, index [, array]])[, thisArg]);
   arrForEachModify: arr => {
     arr.forEach(student => {
-      if (student.grade >= 90) student.grade = 'a';
-      else if (student.grade >= 80) student.grade = 'b';
-      else if (student.grade >= 70) student.grade = 'c';
-      else if (student.grade >= 60) student.grade = 'd';
-      else student.grade = 'f';
+      if (student.grade >= 90) student.grade = "a";
+      else if (student.grade >= 80) student.grade = "b";
+      else if (student.grade >= 70) student.grade = "c";
+      else if (student.grade >= 60) student.grade = "d";
+      else student.grade = "f";
     });
     return arr;
   },
@@ -89,11 +88,11 @@ const functions = {
     */
   arrMap: arr => {
     let newArr = arr.map(student => {
-      if (student.grade >= 90) return { ...student, grade: 'a' };
-      else if (student.grade >= 80) return { ...student, grade: 'b' };
-      else if (student.grade >= 70) return { ...student, grade: 'c' };
-      else if (student.grade >= 60) return { ...student, grade: 'd' };
-      else return { ...student, grade: 'f' };
+      if (student.grade >= 90) return { ...student, grade: "a" };
+      else if (student.grade >= 80) return { ...student, grade: "b" };
+      else if (student.grade >= 70) return { ...student, grade: "c" };
+      else if (student.grade >= 60) return { ...student, grade: "d" };
+      else return { ...student, grade: "f" };
     });
     return newArr;
   },
@@ -133,14 +132,14 @@ const functions = {
     */
   arrLoopsFuncs: [
     {
-      Monica: 'chef',
-      Rachel: 'Personal Buyer',
-      Phoebe: 'Masseuse',
-      Chandler: 'Manager',
-      Ross: 'Paleontologist',
-      Joey: 'Actor'
+      Monica: "chef",
+      Rachel: "Personal Buyer",
+      Phoebe: "Masseuse",
+      Chandler: "Manager",
+      Ross: "Paleontologist",
+      Joey: "Actor"
     },
-    ['fire', 'earth', 'air', 'water'],
+    ["fire", "earth", "air", "water"],
     function forFunc(arr) {
       let newArr = [];
       for (let i = 0; i < arr.length; i++) {
@@ -169,14 +168,14 @@ const functions = {
     function forOfLoop(arr) {
       let newArr = [];
       for (let item of arr) {
-        newArr.push(item + '-');
+        newArr.push(item + "-");
       }
       return newArr;
     },
     function forInLoop(arr) {
       let newArr = [];
       for (let i in arr) {
-        newArr.push(arr[i] + '_');
+        newArr.push(arr[i] + "_");
       }
       return newArr;
     }
@@ -188,8 +187,8 @@ const functions = {
     */
   makeEmailObj: name => {
     let names = [];
-    names.push(name['fname'].toLowerCase());
-    names.push(name['lname'].toLowerCase());
+    names.push(name["fname"].toLowerCase());
+    names.push(name["lname"].toLowerCase());
     let string = `${names[0]}.${names[1]}@evolveu.ca`;
     return string;
   },
@@ -200,7 +199,7 @@ const functions = {
     */
   makeEmailArr: name => {
     let string =
-      name[0].toLowerCase() + '.' + name[1].toLowerCase() + '@evolveu.ca';
+      name[0].toLowerCase() + "." + name[1].toLowerCase() + "@evolveu.ca";
     return string;
   },
 
@@ -225,11 +224,11 @@ const functions = {
 
 // and before this comment ---
 
-functions.assertEquals('a', 'b');
-functions.assertEquals('a', 'a');
+functions.assertEquals("a", "b");
+functions.assertEquals("a", "a");
 functions.assertEquals(1, 2);
 functions.assertEquals(2, 2);
-functions.assertEquals('2', 2);
-functions.assertEquals('This value', 'This value');
+functions.assertEquals("2", 2);
+functions.assertEquals("This value", "This value");
 
 export default functions;
