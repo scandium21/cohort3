392c3b25c38f67d35c160fda0ba86a22b3bd0c96
SC <siliang.chang@gmail.com>
2019-11-24 21:31:53 -0700

    change Pokecard into function component

--- other/comp-props-example/src/Pokecard.js
+++ other/comp-props-example/src/Pokecard.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import './Pokecard.css';
+import React, { Component } from "react";
+import "./Pokecard.css";
 
-class Pokecard extends Component {
-  render() {
-    return (
-      <div className="Pokecard">
-        <h2 style={{ color: 'blue' }}>{this.props.name}</h2>
-        <img src={this.props.image} />
-        <div>Type: {this.props.type}</div>
-      </div>
-    );
-  }
+function Pokecard(props) {
+  return (
+    <div className="Pokecard">
+      <h2 style={{ color: "blue" }}>{props.name}</h2>
+      <img src={props.image} />
+      <div>Type: {props.type}</div>
+    </div>
+  );
 }
 
 export default Pokecard;
