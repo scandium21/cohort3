0849b228d90bbae781dfa033629ddeb974612291
Siliang Chang <siliang.chang@gmail.com>
2019-12-02 19:04:29 -0700

    rename stack and queue

---
+++ react-02/src/components/Stack & Queue/Queue.js
@@ -0,0 +1,20 @@
+import { SinglyLinkedList } from "../Linked List/SinglyLinkedList";
+
+export class Queue extends SinglyLinkedList {
+  constructor(subject, amount) {
+    super(subject, amount);
+  }
+  delete() {
+    let deleted = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    return deleted;
+  }
+  push(subject, amount) {
+    let currNode = this.head;
+    while (currNode.next) {
+      currNode = currNode.next;
+    }
+    super.insert(currNode, subject, amount);
+  }
+}
---
+++ react-02/src/components/Stack & Queue/Stack.js
@@ -0,0 +1,16 @@
+import { SinglyLinkedList } from "../Linked List/SinglyLinkedList";
+
+export class Stack extends SinglyLinkedList {
+  constructor(subject, amount) {
+    super(subject, amount);
+  }
+  delete() {
+    let deleted = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    return deleted;
+  }
+  push(subject, amount) {
+    super.addFront(subject, amount);
+  }
+}
