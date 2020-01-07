baa8c8ce7d56a44f2ea42ba3988f4286ddc1626d
Siliang Chang <siliang.chang@gmail.com>
2020-01-07 00:41:15 -0700

    cantax written in python

--- src/python/comp200/tests/cantax_test.py
+++ src/python/comp200/tests/cantax_test.py
@@ -1,4 +1,13 @@
 import pytest
 import sys
 sys.path.insert(0, '../')
-from cantax import SinglyLinkedList
+from cantax import fed_tax
+
+
+def test_cantax():
+    assert round(fed_tax(47630), 2) == 7144.50
+    assert round(fed_tax(1), 2) == 0.15
+    assert round(fed_tax(2), 2) == 0.30
+    assert round(fed_tax(50000), 2) == 7630.35
+    assert round(fed_tax(150000), 2) == 31211.10
+    assert round(fed_tax(250000), 2) == 61796.25
