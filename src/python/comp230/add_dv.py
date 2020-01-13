33e92501df238b9e5b6305f3230d19968b08dbeb
Siliang Chang <siliang.chang@gmail.com>
2020-01-12 17:14:00 -0700

    data validation added

---
+++ src/python/comp230/add_dv.py
@@ -0,0 +1,97 @@
+from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl import Workbook
+from openpyxl.utils import quote_sheetname
+from openpyxl import load_workbook
+import datetime
+
+# prime ids: non-negative whole numbers and no duplicates
+# prices and payments: non-negative numbers
+# quantities, non-prime ids: non-negative whole numbers
+# customer, product names: less than 20 character strings
+# customer emails: <50 char string
+# date: valid date format
+# active: 0 (not active) or 1 (active)
+
+dv_non_neg_whole = DataValidation(type='whole',
+                                  operator='greaterThanOrEqual',
+                                  formula1=0)
+dv_date = DataValidation(type='date')
+dv_string_len20 = DataValidation(type="textLength",
+                                 operator="lessThanOrEqual",
+                                 formula1=20)
+dv_string_len50 = DataValidation(type="textLength",
+                                 operator="lessThanOrEqual",
+                                 formula1=50)
+dv_0_or_1 = DataValidation(type='whole',
+                           operator='between',
+                           formula1=0,
+                           formula2=1)
+dv_non_neg = DataValidation(type='decimal',
+                            operator='greaterThanOrEqual',
+                            formula1=0)
+
+dvs = [
+    dv_0_or_1, dv_date, dv_non_neg_whole, dv_string_len50, dv_string_len20,
+    dv_non_neg
+]
+
+prime_ids = {'invoices': 'i_id', 'customers': 'c_id', 'products': 'p_id'}
+non_prime_ids = {
+    'invoices': 'c_id',
+    'invoice_details': 'i_id, p_id',
+    'customers': '',
+    'products': ''
+}
+
+wb = load_workbook('sample_db.xlsx')
+
+for sheetname in wb.sheetnames:
+    ws = wb[sheetname]
+    for col in ws[1]:
+        header = col.value
+        if header.find('id') != -1:
+            if non_prime_ids.get(col.parent.title).find(col.value) != -1:
+                # print(col.parent.title, col.value, col.column_letter)
+                dv_non_neg_whole.add(
+                    f'{col.column_letter}2:{col.column_letter}1048576')
+                ws.add_data_validation(dv_non_neg_whole)
+            else:
+                # print("prime", col.parent.title, col.value, col.column_letter)
+                no_dup_range = f'=countif({col.column_letter}:{col.column_letter}, {col.column_letter}2)<=1'
+                no_dup_cells = f'{col.column_letter}2:{col.column_letter}1048576'
+                dv_no_dup = DataValidation(type='custom',
+                                           formula1=no_dup_range)
+                dv_no_dup.add(no_dup_cells)
+                ws.add_data_validation(dv_no_dup)
+                dvs.append(dv_no_dup)
+        elif header.find('date') != -1:
+            dv_date.add(f'{col.column_letter}2:{col.column_letter}1048576')
+            ws.add_data_validation(dv_date)
+        elif header.find('name') != -1:
+            dv_string_len20.add(
+                f'{col.column_letter}2:{col.column_letter}1048576')
+            ws.add_data_validation(dv_string_len20)
+        elif header.find('email') != -1:
+            dv_string_len50.add(
+                f'{col.column_letter}2:{col.column_letter}1048576')
+            ws.add_data_validation(dv_string_len50)
+        elif header.find('quant') != -1:
+            dv_non_neg_whole.add(
+                f'{col.column_letter}2:{col.column_letter}1048576')
+            ws.add_data_validation(dv_non_neg_whole)
+        elif header.find('payment') != -1 or header.find('price') != -1:
+            dv_non_neg.add(f'{col.column_letter}2:{col.column_letter}1048576')
+            ws.add_data_validation(dv_non_neg)
+        elif header.find('active') != -1:
+            dv_0_or_1.add(f'{col.column_letter}2:{col.column_letter}1048576')
+            ws.add_data_validation(dv_0_or_1)
+
+for dv in dvs:
+    dv.errorTitle = 'Invalid Entry'
+    if type(dv.formula1) == str and dv.formula1.find('countif') != -1:
+        dv.error = 'Duplicate entry'
+    else:
+        dv.error = f'Need to enter data of type: {dv.type}. ' + f'And its value needs to: {dv.operator} {dv.formula1}' + (
+            f' and {dv.formula2}' if dv.formula2 != None else '')
+
+wb.save('sample_db.xlsx')
--- src/python/comp230/dv_ex.py
+++ src/python/comp230/dv_ex.py
@@ -5,6 +5,7 @@ wb = Workbook()
 ws = wb.active
 # Create a data-validation object with list validation
 dv = DataValidation(type="list", formula1='"Dog,Cat,Bat"', allow_blank=True)
+dv2 = DataValidation(type='date')
 # Optionally set a custom error message
 dv.error = 'Your entry is not in the list'
 dv.errorTitle = 'Invalid Entry'
@@ -13,6 +14,7 @@ dv.prompt = 'Please select from the list'
 dv.promptTitle = 'List Selection'
 # Add the data-validation object to the worksheet
 ws.add_data_validation(dv)
+ws.add_data_validation(dv2)
 # Create some cells, and add them to the data-validation object
 c1 = ws["A1"]
 c1.value = "Dog"
@@ -22,6 +24,7 @@ c2.value = "An invalid value"
 dv.add(c2)
 # Or, apply the validation to a range of cells
 dv.add('B1:B1048576')  # This is the same as for the whole of column B
+dv2.add('C1:C1048576')  # This is the same as for the whole of column B
 # Check with a cell is in the validator
 print("B4" in dv)
 wb.save('dv_ex.xlsx')
