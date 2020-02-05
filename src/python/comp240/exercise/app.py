7cb4dfabd29daf10cd7b6f665f50bf094ff56b21
Siliang Chang <siliang.chang@gmail.com>
2020-02-04 21:19:58 -0700

    basic flask to show info from comp230 worksheet

---
+++ src/python/comp240/exercise/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request, render_template
+
+from read_data import map_wb_to_dict
+
+app = Flask(__name__)
+
+wb = map_wb_to_dict()
+
+# simple dump
+@app.route("/")
+def simple_dumps():
+    return wb
+
+
+def get_ws_header(wb=wb):
+    ws_headers = {}
+    for ws, items in wb.items():
+        for header in items[0].keys():
+            ws_headers[ws] = ws_headers.get(ws, [])
+            ws_headers[ws].append(header)
+    return ws_headers
+
+
+@app.route("/template")
+def template_loop():
+    return render_template("index.html", results=wb, ws_headers=get_ws_header())
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
