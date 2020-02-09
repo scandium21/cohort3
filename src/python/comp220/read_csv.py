5ce765fbaf8e2aa1671bab42a23063f37a060e74
Siliang Chang <siliang.chang@gmail.com>
2020-02-08 22:46:22 -0700

    comp220: modify dict structure to loop through file once

--- src/python/comp220/read_csv.py
+++ src/python/comp220/read_csv.py
@@ -2,7 +2,7 @@ def get_category_idx_pair(header):
     return {cat.rstrip(): ind for ind, cat in enumerate(header.split(","))}
 
 
-def get_info_by_category(target, cat):
+def get_info_by_category(target, cats):
     sum_target = {}
     lines_num = {}
 
@@ -10,48 +10,92 @@ def get_info_by_category(target, cat):
         for idx, line in enumerate(csv):
             if idx == 0:
                 header_idx = get_category_idx_pair(line)
-                cat_idx = header_idx[cat]
+                cat_idx = {}
                 target_idx = header_idx[target]
+                for cat in cats:
+                    cat_idx[cat] = header_idx[cat]
+                    sum_target[cat] = {}
+                    lines_num[cat] = {}
             else:
                 line_data = line.split(",")
-                sub_cat = line_data[cat_idx]
                 target_val = int(line_data[target_idx])
-                sum_target[sub_cat] = sum_target.get(sub_cat, 0) + target_val
-                lines_num[sub_cat] = lines_num.get(sub_cat, 0) + 1
-    return {
-        sub_cat: [num_line, target_sum]
-        for sub_cat, num_line, target_sum in zip(
-            sum_target.keys(), sum_target.values(), lines_num.values()
+                sub_cats = {}
+                for cat in cats:
+                    sub_cats[cat] = line_data[cat_idx[cat]]
+                    sum_target[cat][sub_cats[cat]] = (
+                        sum_target[cat].get(sub_cats[cat], 0) + target_val
+                    )
+                    lines_num[cat][sub_cats[cat]] = (
+                        lines_num[cat].get(sub_cats[cat], 0) + 1
+                    )
+    return sum_target, lines_num
+
+
+# print(get_info_by_category("RES_CNT", ["CLASS", "SECTOR"]))
+# (
+#     {
+#         "CLASS": {
+#             "Residential": 1263734,
+#             "Industrial": 922,
+#             "Major Park": 0,
+#             "Residual Sub Area": 0,
+#         },
+#         "SECTOR": {
+#             "CENTRE": 199977,
+#             "SOUTH": 227345,
+#             "NORTHEAST": 185534,
+#             "NORTHWEST": 174126,
+#             "NORTH": 160502,
+#             "SOUTHEAST": 135009,
+#             "EAST": 57666,
+#             "WEST": 124497,
+#         },
+#     },
+#     {
+#         "CLASS": {
+#             "Residential": 208,
+#             "Industrial": 42,
+#             "Major Park": 4,
+#             "Residual Sub Area": 49,
+#         },
+#         "SECTOR": {
+#             "CENTRE": 62,
+#             "SOUTH": 58,
+#             "NORTHEAST": 44,
+#             "NORTHWEST": 30,
+#             "NORTH": 30,
+#             "SOUTHEAST": 23,
+#             "EAST": 23,
+#             "WEST": 33,
+#         },
+#     },
+# )
+
+
+def create_report_str(target, cats):
+    string = ""
+    sum_target, sum_lines = get_info_by_category(target, cats)
+    for cat in cats:
+        string += f"======================= Calculation by {cat} =========================\n\n"
+        string += (
+            "Sub Category".ljust(30)
+            + f"Number of Lines in File".ljust(30)
+            + f"Sum of {target}\n\n"
         )
-    }
+        for sub_cat, target_sum, target_lines in zip(
+            sum_target[cat].keys(), sum_target[cat].values(), sum_lines[cat].values()
+        ):
+            string += (
+                f"{sub_cat}".ljust(30) + f"{target_sum}".ljust(30) + f"{target_lines}\n"
+            )
+        string += "\n\n"
 
+    string += f"\ncsv file sourse: https://data.calgary.ca/Demographics/Census-by-Community-2018/cc4n-ndvs"
 
-print(get_info_by_category("RES_CNT", "CLASS"))
-# {'Residential': [1263734, 208], 'Industrial': [922, 42], 'Major Park': [0, 4], 'Residual Sub Area': [0, 49]}
-
-
-def create_report_str(cat, target):
-    string = (
-        f"======================= Calculation by {cat} =========================\n\n"
-    )
-    string += (
-        "Sub Category".ljust(30)
-        + f"Number of Lines in File".ljust(30)
-        + f"Sum of {target}\n\n"
-    )
-    result = get_info_by_category(target, cat)
-    for sub_cat, data in zip(result.keys(), result.values()):
-        string += f"{sub_cat}".ljust(30) + f"{data[1]}".ljust(30) + f"{data[0]}\n"
+    with open("csv_report.txt", "w") as report:
+        report.write(string)
     return string + "\n\n"
 
 
-final_string = create_report_str("CLASS", "RES_CNT") + create_report_str(
-    "SECTOR", "RES_CNT"
-)
-final_string += f"\n\ncsv file sourse: https://data.calgary.ca/Demographics/Census-by-Community-2018/cc4n-ndvs"
-
-try:
-    csv_report = open("csv_report.txt", "w")
-    csv_report.write(final_string)
-finally:
-    csv_report.close()
+if __name__ == "__main__":
+    create_report_str("RES_CNT", ["CLASS", "SECTOR"])
